d9cfc750340686bfa3a1fff78701b8540fcaa0a4
Prosonul Haque <proshonulhaq@gmail.com>
2025-06-15 13:04:05 +0600

    Used loading skeleton instead of animated spin

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,8 +1,5 @@
+import DashboardSkeleton from "../ui/skeletons";
+
 export default function Loading() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-600"></div>
-      <span className="ml-4 text-lg">Loading...</span>
-    </div>
-  );
+  return <DashboardSkeleton />;
 }
