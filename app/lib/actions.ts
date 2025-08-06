ebd8bcbba9be54300cb72d104469d562e22f3d48
Prosonul Haque <proshonulhaq@gmail.com>
2025-08-06 16:52:36 +0600

    Added user authentication mechanism

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 import postgres from 'postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -64,38 +66,59 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 export async function updateInvoice(id: string, prevState: State, formData: FormData) {
-    const validatedFields = UpdateInvoice.safeParse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    });
-    
-    if(!validatedFields.success){
-      return {
-        errors: validatedFields.error.flatten().fieldErrors,
-        message: "Missing Fields. Failed to Update Invoice."
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice."
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  
+  try{
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  }
+  catch(error){
+    console.error('Error updating invoice:', error);
+  }
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string){
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+){
+  try{
+    signIn('credentials', formData);
+  }
+  catch(error){
+    if (error instanceof(AuthError)){
+      switch (error.type){
+        case 'CredentialsSignin':
+          return "Invalid Credentials"
+        default:
+          return "Something went wrong"
       }
     }
 
-    const { customerId, amount, status } = validatedFields.data;
-    const amountInCents = amount * 100;
-   
-    try{
-      await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
-    }
-    catch(error){
-      console.error('Error updating invoice:', error);
-    }
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    throw error;
   }
-
-  export async function deleteInvoice(id: string){
-    await sql`DELETE FROM invoices WHERE id = ${id}`
-    revalidatePath('/dashboard/invoices')
-  }
+}
